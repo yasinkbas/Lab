15c2ff27b320df12d6655bfbbaf5c527a132b13f
yasinkbas <yasin.kbas12@gmail.com>
2020-10-16 20:02:32 +0300

    remove outdated files

--- Example/Lab/AppDelegate.swift
+++ Example/Lab/AppDelegate.swift
@@ -14,9 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainBuilder().build()
-        window?.makeKeyAndVisible()
+
         return true
     }
 
